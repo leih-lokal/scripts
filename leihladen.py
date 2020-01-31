c3cc35d91feccc96275442b1fffc837cfce0e9fa
skjerns <14980558+skjerns@users.noreply.github.com>
2020-01-31 15:23:30 +0100

    Update leihladen.py

--- leihladen.py
+++ leihladen.py
@@ -17,6 +17,17 @@ from email.header import decode_header
 
 ############ SETTINGS ################################################ 
 def get_reminder_template(customer, rental):
+    
+    string = 'Liebe/r {customer.firstname} {customer.lastname}.\n\n' \
+             'Danke, dass Sie Ausleiher/in im leih.lokal sind.\n\n'\
+              'Wir möchten Sie daran erinnern, den am {rental.rented_on} ausgeliehenen Gegenstand ({rental.item_name} ({rental.item_id})) wieder abzugeben. '\
+              'Der bei uns vermerkte Rückgabetermin war der {rental.to_return_on}.
+
+              'Zum heutigen Zeitpunkt fallen 2 Euro an, die unserer Spendenkasse zugeführt werden. '\
+              'Wie Sie unseren Nutzungsbedingungen entnehmen können, kommt pro Öffnungstag eine kleine Säumnisgebühr von 2 Euro je Gegenstand dazu. '\
+              'Bei Fragen wenden Sie sich bitte via E-Mail an leih.lokal@buergerstiftung-karlsruhe.de oder telefonisch während der Öffnungszeiten unter 0721/47004551 an unsere Mitarbeiter.\n\n'\
+              'Grüße aus dem leih.lokal\n\nÖffnungszeiten: Mo, Do, Fr: 15-19, Sa: 11-16'
+
     return f'Hey {customer.firstname} {customer.lastname} please return {rental.item_name}'
 
 CUSTOMER_DELETION_NOTIFICATION_TITLE = "{} Kunde(n) sollten manuell gelöscht werden"
