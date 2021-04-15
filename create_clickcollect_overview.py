3fff61be659a8789201c141810b18f2456048f99
leihlokal-laptop <github@buergerstiftung-karlsruhe.de>
2021-04-15 15:34:16 +0200

    formatting

--- create_clickcollect_overview.py
+++ create_clickcollect_overview.py
@@ -217,9 +217,13 @@ if __name__=='__main__':
     df_selected.set_index('Zeit', inplace=True)
     df_selected['Eingetragen?'] = ['[   ]']*len(df_selected)
 
-    row = ['', '* Nutzernummer und Pfandbeträge automatisch inferiert. Kann fehlerhaft sein!\nEs werden sämtliche im Textfeld enthaltene Zahlen als Gegenstandsnummern interpretiert.', *(len(df_selected.columns)-2)*['']]
+    row = ['', '* Nutzernummer und Pfandbeträge automatisch inferiert. Kann fehlerhaft sein!', *(len(df_selected.columns)-2)*['']]
+    row2 = ['', 'Es werden sämtliche Zahlen im Textfeld als Gegenstandsnummern interpretiert und das Pfand in Klammern geschrieben.', *(len(df_selected.columns)-2)*['']]
+
     df_selected.loc[len(df_selected)] = row
-    df_selected.index = df_selected.index.to_list()[:-1]+ ['']
+    df_selected.loc[len(df_selected)] = row2
+
+    df_selected.index = df_selected.index.to_list()[:-2]+ ['', '']
 
     xls_file = choose_filesave(default_dir=os.path.expanduser('~\\Desktop\\'), default_filename=f'CC-Übersicht-{date_str}.xlsx')
 
