5f7b950e732387fe50a1046275be077e58d0cba8
skjerns <14980558+skjerns@users.noreply.github.com>
2022-04-09 08:37:47 +0200

    run at 21UTC, clear after 19GMT

--- WPAppointmentManager/main.py
+++ WPAppointmentManager/main.py
@@ -95,7 +95,7 @@ for appointment in pending_appointments:
 # reset status to instock for items that have been reserverd but not rented
 # run this only if it is after 20:00 in the evening. The cron job
 # is set to run somehwere after that, so that should clear the things of the day.
-if datetime.now(pytz.timezone('Europe/Berlin')).time() > time(20, 0, tzinfo=pytz.timezone('Europe/Berlin')):
+if datetime.now(pytz.timezone('Europe/Berlin')).time() > time(19, 0, tzinfo=pytz.timezone('Europe/Berlin')):
     appointments_of_today = list(
         filter(lambda appointment: appointment["time_end"].date() == datetime.today().date(), appointments))
     appointments_after_today = list(
