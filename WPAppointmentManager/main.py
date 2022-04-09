7d5d7e55f15e72b2d63ffe93c2410206ae04ab0b
skjerns <14980558+skjerns@users.noreply.github.com>
2022-04-09 08:34:33 +0200

    change cronjob to run at 4

--- WPAppointmentManager/main.py
+++ WPAppointmentManager/main.py
@@ -93,7 +93,9 @@ for appointment in pending_appointments:
     logging.info(reason)
 
 # reset status to instock for items that have been reserverd but not rented
-if datetime.now(pytz.timezone('Europe/Berlin')).time() > time(20, 0, tzinfo=pytz.timezone('Europe/Berlin')):
+# run this only if it is before 10:00 in the morning. The cron job
+# is set to run somehwere in the morning, so this should only run once.
+if datetime.now(pytz.timezone('Europe/Berlin')).time() < time(10, 0, tzinfo=pytz.timezone('Europe/Berlin')):
     appointments_of_today = list(
         filter(lambda appointment: appointment["time_end"].date() == datetime.today().date(), appointments))
     appointments_after_today = list(
