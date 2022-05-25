f32e62cb4dde3092f65e7889dfe5bdb60e2b041d
skjerns <14980558+skjerns@users.noreply.github.com>
2022-05-25 11:31:20 +0200

    change Sie zu Du

--- WPAppointmentManager/appointment_parser.py
+++ WPAppointmentManager/appointment_parser.py
@@ -27,9 +27,9 @@ def _clean_data(appointment):
         # "other_appointment": appointment[
         #                          'Haben Sie schon einen weiteren/anderen Termin fr diesen Tag gebucht?'] == "Ja",
         "status": appointment["cancelled"], # Attended / Genehmigt / Zusammengelegt / Cancelled by customer / Rejected / Cancelled / Bitte ansehen
-        "customer_name": appointment['Ihr Vor- und Zuname'],
-        "customer_id": appointment.get('Ihre Nutzernummer (falls zur Hand)'),
-        "customer_mail": appointment['Ihre Email'],
+        "customer_name": appointment['Dein Vor- und Zuname'],
+        "customer_id": appointment.get('Deine Nutzernummer (falls zur Hand)'),
+        "customer_mail": appointment['Deine Email'],
         "appointment_id": appointment['itemnumber']
     }
 
