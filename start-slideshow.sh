b0c1f7d28d555d5b27af10f7b45524838f66f506
Ferdinand Mütsch <ferdinand@muetsch.io>
2023-10-17 10:12:27 +0200

    fix: update slideshow script

--- start-slideshow.sh
+++ start-slideshow.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sleep 5
-counter=1
-while [ $counter -le 5 ]
-do
-   sleep 3
-   sudo -u pi okular /home/pi/raspberry-pi-fenster.pdf --presentation
-done
+# Convert PDF to pictures
+# pdftoppm -png /home/pi/slideshow/raspberry-pi-fenster.pdf /home/pi/slideshow/
 
-echo Abgestürzt, bitte Stecker ziehen und neu einstecken.
-read EINGABE
+# Start slideshow
+feh -Y -x -q -D 5 -B black -F -Z -z -r slideshow/
+
+### OLD ###
+# (Okular stopped working on Pi due to some shared library error)
+# sleep 5
+# counter=1
+# while [ $counter -le 5 ]
+# do
+#    sleep 3
+#        sudo -u pi okular /home/pi/raspberry-pi-fenster.pdf --presentation
+# done
+#
+# echo Abgestürzt, bitte Stecker ziehen und neu einstecken.
+# read EINGABE
